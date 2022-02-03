d60d7ab5bc6c35dc7f45c3fb428e81e31baced0b
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-02-03 10:37:44 +0100

    Remove empty swagger

--- src/Controllers/ConsultationAPIController.php
+++ src/Controllers/ConsultationAPIController.php
@@ -2,9 +2,7 @@
 
 namespace EscolaLms\Consultations\Http\Controllers;
 
-use EscolaLms\Consultations\Http\Controllers\Swagger\ConsultationAPISwagger;
-
-class ConsultationAPIController extends AppBaseController implements ConsultationAPISwagger
+class ConsultationAPIController extends AppBaseController
 {
 
 }
--- src/Controllers/Swagger/ConsultationAPISwagger.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace EscolaLms\Consultations\Http\Controllers\Swagger;
-
-interface ConsultationAPISwagger
-{
-
-}
