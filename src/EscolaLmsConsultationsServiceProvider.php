5eb1dfb360742039305e9ded28fd2750e6701d20
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-02-17 18:34:43 +0100

    Repair bug in register settingsProvider

--- src/EscolaLmsConsultationsServiceProvider.php
+++ src/EscolaLmsConsultationsServiceProvider.php
@@ -8,8 +8,8 @@ use EscolaLms\Consultations\Repositories\Contracts\ConsultationRepositoryContrac
 use EscolaLms\Consultations\Repositories\Contracts\ConsultationTermsRepositoryContract;
 use EscolaLms\Consultations\Services\ConsultationService;
 use EscolaLms\Consultations\Services\Contracts\ConsultationServiceContract;
-use EscolaLms\Courses\Providers\SettingsServiceProvider;
 use EscolaLms\Jitsi\EscolaLmsJitsiServiceProvider;
+use EscolaLms\Settings\EscolaLmsSettingsServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -49,6 +49,6 @@ class EscolaLmsConsultationsServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/config.php', 'escolalms_consultations');
         $this->app->register(AuthServiceProvider::class);
         $this->app->register(EscolaLmsJitsiServiceProvider::class);
-        $this->app->register(SettingsServiceProvider::class);
+        $this->app->register(EscolaLmsSettingsServiceProvider::class);
     }
 }
