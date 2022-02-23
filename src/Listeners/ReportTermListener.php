553caedcc133e79a1f9b564300ac8829a560892f
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-02-23 12:40:52 +0100

    Force cart version

--- src/Listeners/ReportTermListener.php
+++ src/Listeners/ReportTermListener.php
@@ -2,7 +2,7 @@
 
 namespace EscolaLms\Consultations\Listeners;
 
-use EscolaLms\Cart\Events\OrderPaid;
+use EscolaLms\Cart\Events\CartOrderPaid;
 use EscolaLms\Consultations\Services\Contracts\ConsultationServiceContract;
 
 class ReportTermListener
@@ -15,7 +15,7 @@ class ReportTermListener
         $this->consultationServiceContract = $consultationServiceContract;
     }
 
-    public function handle(OrderPaid $event)
+    public function handle(CartOrderPaid $event)
     {
         $this->consultationServiceContract->setPivotOrderConsultation($event->getOrder(), $event->getUser());
     }
