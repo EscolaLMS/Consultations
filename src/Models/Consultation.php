624e9c6f0fee14d41b926586f9707fc517d819e3
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-04-12 16:11:31 +0200

    Add trim

--- src/Models/Consultation.php
+++ src/Models/Consultation.php
@@ -206,7 +206,7 @@ class Consultation extends Model
     public function getImageUrlAttribute(): string
     {
         if ($this->attributes['image_path'] ?? null) {
-            $path = trim($this->attributes['image_path'], '/');
+            $path = trim(trim($this->attributes['image_path'], '/'));
             if ($path) {
                 $imagePath = Storage::url($path);
                 return preg_match('/^(http|https):.*$/', $imagePath, $oa) ?
@@ -220,7 +220,7 @@ class Consultation extends Model
     public function getLogotypeUrlAttribute(): string
     {
         if ($this->attributes['logotype_path'] ?? null) {
-            $path = trim($this->attributes['image_path'], '/');
+            $path = trim(trim($this->attributes['image_path'], '/'));
             if ($path) {
                 $logotype = Storage::url(trim($this->attributes['logotype_path'], '/'));
                 return preg_match('/^(http|https):.*$/', $logotype, $oa) ?
