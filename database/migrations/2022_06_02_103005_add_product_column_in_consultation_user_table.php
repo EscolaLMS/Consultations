7b443fa48c3341cc1c651b9e872d587715278891
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-06-02 13:24:08 +0200

    Add related product for pivot

---
+++ database/migrations/2022_06_02_103005_add_product_column_in_consultation_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProductColumnInConsultationUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('consultation_user', function (Blueprint $table) {
+            $table->bigInteger('product_id')->unsigned()->nullable();
+            $table->foreign('product_id')->on('products')->references('id')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('consultation_user', function (Blueprint $table) {
+            $table->dropColumn('product_id');
+        });
+    }
+}
