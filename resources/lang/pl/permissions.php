7a8df24db4afe9cabda51aede4193c2c0304d9f9
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-11-18 15:38:55 +0100

    Add translations (#90)

---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+use EscolaLms\Consultations\Enum\ConsultationsPermissionsEnum;
+
+return [
+    ConsultationsPermissionsEnum::CONSULTATION_LIST => 'Lista konsultacji',
+    ConsultationsPermissionsEnum::CONSULTATION_CREATE => 'Utwórz konsultację',
+    ConsultationsPermissionsEnum::CONSULTATION_UPDATE => 'Aktualizuj konsultację',
+    ConsultationsPermissionsEnum::CONSULTATION_DELETE => 'Usuń konsultację',
+    ConsultationsPermissionsEnum::CONSULTATION_READ => 'Wyświetl konsultację',
+    ConsultationsPermissionsEnum::CONSULTATION_CHANGE_TERM => 'Zmień termin konsultacji',
+];
