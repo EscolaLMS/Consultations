59d9b163918fd43da51301ea67bdad29b20a5f6c
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2023-01-13 13:01:29 +0100

    Change value in config

--- src/config.php
+++ src/config.php
@@ -6,8 +6,8 @@ return [
     'perPage' => 15,
 
     'modifier_date' => [
-        ConsultationTermReminderStatusEnum::REMINDED_HOUR_BEFORE => '+1 minute',
-        ConsultationTermReminderStatusEnum::REMINDED_DAY_BEFORE => '+1 hour',
+        ConsultationTermReminderStatusEnum::REMINDED_HOUR_BEFORE => '+1 hour',
+        ConsultationTermReminderStatusEnum::REMINDED_DAY_BEFORE => '+1 day',
     ],
     'exclusion_reminder_status' => [
         ConsultationTermReminderStatusEnum::REMINDED_DAY_BEFORE => [
