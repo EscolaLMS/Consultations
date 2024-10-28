b953b2b112389b2efb8346c2638d3957f53c0677
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-10-28 12:27:13 +0100

    Fix

--- src/Dto/ConsultationDto.php
+++ src/Dto/ConsultationDto.php
@@ -38,7 +38,7 @@ class ConsultationDto extends BaseDto implements ModelDtoContract
     public function getImagePath()
     {
         if ($this->imagePath !== false) {
-            return $this->imagePath === null ? '' : Str::after($this->imagePath, env('AWS_URL') . '/');
+            return $this->imagePath === null ? '' : Str::after($this->imagePath, Str::after(env('AWS_URL'), 'https://') . '/');
         }
         return false;
     }
@@ -47,7 +47,7 @@ class ConsultationDto extends BaseDto implements ModelDtoContract
     {
         if ($this->logotypePath !== false) {
             if ($this->logotypePath) {
-                $logotypePath = Str::after($this->logotypePath, env('AWS_URL') . '/');
+                $logotypePath = Str::after($this->logotypePath, Str::after(env('AWS_URL'), 'https://') . '/');
                 return Str::startsWith($logotypePath, ConstantEnum::DIRECTORY) ? $logotypePath : ConstantEnum::DIRECTORY . '/' .$logotypePath;
             }
             return '';
