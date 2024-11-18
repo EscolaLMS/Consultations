9c9e3bdbad6653d00f96348c4d639ec83e974337
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-11-18 11:36:11 +0100

    Fix

--- src/Services/ConsultationService.php
+++ src/Services/ConsultationService.php
@@ -487,7 +487,7 @@ class ConsultationService implements ConsultationServiceContract
                     /** @var ConsultationUserTerm $consultationUserTerm */
                     $consultationUserTerm = $this->consultationUserTermRepository->update([
                         'executed_at' => $dto->getExecutedAt(),
-                        'executed_status' => ConsultationTermStatusEnum::REPORTED,
+                        'executed_status' => $dto->getAccept() ? ConsultationTermStatusEnum::APPROVED : ConsultationTermStatusEnum::REPORTED,
                     ], $userTerm->getKey());
 
                     if (!$consultationUserTerm->consultationUser->user) {
